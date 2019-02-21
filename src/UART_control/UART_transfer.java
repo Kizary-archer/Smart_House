48a672c1f83b3e805f0090f3716430708f0eca74
Unknown <Kizary-archer@yandex.ru>
2019-02-21 21:19:27 +0300

    jssc

--- src/UART_control/UART_transfer.java
+++ src/UART_control/UART_transfer.java
@@ -1,4 +1,15 @@
 package UART_control;
 
+import jssc.SerialPortList;
+
 public class UART_transfer {
+
+    public static void main(String[] args) {
+        String[] portNames = SerialPortList.getPortNames();
+        for(int i = 0; i < portNames.length; i++){
+            System.out.println(portNames[i]);
+        }
+    }
+
 }
+
